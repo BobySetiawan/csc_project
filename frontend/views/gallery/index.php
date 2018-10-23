c054b3386760d7cd4a1103feb53f1fbbb51e9bfe
BobySetiawan <setiawanboby386@gmail.com>
2018-10-23 13:56:30 +0700

    update 23 oktober 2018

--- frontend/views/gallery/index.php
+++ frontend/views/gallery/index.php
@@ -2,67 +2,23 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+//use frontend\models\gallery;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\GallerySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Galleries';
+$this->title = 'Gallery CSC';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="gallery-index">
-
-   
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-     <div class="row">
-        <div class="col-xs-12">
-            <div class="box box-danger">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Data Sejarah</h3> <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'layout'=>'{summary}',]); ?>
-                        </div>
-                            <div class="box-body">
-                                <div class="box-body table-responsive no-padding">
-                                    <div class="table table-hover">
-
-
-    <p>
-        <?= Html::a('Create Gallery', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <!-- <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'Kd_Gallery',
-            'Judul',
-            'Foto',
-            'Tanggal',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-        'layout' => '{items}',
-                            ]); ?>
-                            <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'layout'=>'{pager}',]); ?> -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </div>
-</div>
 
 <ul class="timeline">
 
     <!-- timeline time label -->
+    <?php foreach ($dataProvider->models as $model) { ?>
     <li class="time-label">
         <span class="bg-red">
-            10 Feb. 2014
+            <?php echo $model->Tanggal; ?>
         </span>
     </li>
     <!-- /.timeline-label -->
@@ -74,18 +30,19 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="timeline-item">
            <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
 
-            <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+            <h3 class="timeline-header"><a href="#">Admin </a> uploaded new photos</h3>
 
             <div class="timeline-body">
-                ...
-                Content goes here
+                 <img src="<?= Yii::$app->request->baseUrl . '/images/' . $model->Foto ?>" class=" img-responsive" >
+               
             </div>
 
             <div class="timeline-footer">
-                <a class="btn btn-primary btn-xs">...</a>
+                <a class="btn btn-primary btn-xs"><?php echo $model->Judul; ?></a>
             </div>
         </div>
     </li>
+    <?php } ?>
     <li>
         <i class="fa fa-clock-o bg-gray"></i>
     </li>
